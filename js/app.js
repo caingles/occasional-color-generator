5c351be0924aa5f425fb590ee8213d7d0322193b
Christian Ingles <christianarguetaaa@gmail.com>
2022-12-12 20:33:20 -0600

    Getting the elements and storing them in variables

---
+++ js/app.js
@@ -0,0 +1,6 @@
+const btnColorRandom = document.querySelector('#btnColorRandom');
+const btnColorSimple = document.querySelector('#colorSimple');
+const btnColorRgb = document.querySelector('#colorRgb');
+const btnColorHexa = document.querySelector('#colorHexa');
+const colorText = document.querySelector('#colorText');
+let conmutador = 'btnColorSimple';
