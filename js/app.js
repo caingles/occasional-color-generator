a42f4a16169db950f10385c7dd66d4ad1d3dfc83
Christian Ingles <christianarguetaaa@gmail.com>
2022-12-12 20:34:14 -0600

    Adding variables and the array of colors

--- js/app.js
+++ js/app.js
@@ -4,3 +4,24 @@ const btnColorRgb = document.querySelector('#colorRgb');
 const btnColorHexa = document.querySelector('#colorHexa');
 const colorText = document.querySelector('#colorText');
 let conmutador = 'btnColorSimple';
+
+const colorsText = [
+    'Red',
+    'Yellow',
+    'Pink',
+    'LightSeaGreen',
+    'Aqua',
+    'PaleTurquoise',
+    'Turquoise',
+    'MediumTurquoise',
+    'MediumSlateBlue',
+    'Blue',
+    'BurlyWood',
+    'SandyBrown',
+    'Chocolate',
+    'Brown',
+    'Maroon',
+    'LightSalmon',
+  ];
+  
+  let number;
