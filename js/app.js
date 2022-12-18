692ce62e49d61d5154394cebaa957fef96d0727c
Christian Ingles <christianarguetaaa@gmail.com>
2022-12-18 11:21:29 -0600

    Formatting code with prettier

--- js/app.js
+++ js/app.js
@@ -1,68 +1,68 @@
-const btnColorRandom = document.querySelector('#btnColorRandom');
-const btnColorSimple = document.querySelector('#colorSimple');
-const btnColorRgb = document.querySelector('#colorRgb');
-const btnColorHexa = document.querySelector('#colorHexa');
-const colorText = document.querySelector('#colorText');
-let conmutador = 'btnColorSimple';
+const btnColorRandom = document.querySelector("#btnColorRandom");
+const btnColorSimple = document.querySelector("#colorSimple");
+const btnColorRgb = document.querySelector("#colorRgb");
+const btnColorHexa = document.querySelector("#colorHexa");
+const colorText = document.querySelector("#colorText");
+let conmutador = "btnColorSimple";
 
 const colorsText = [
-    'Red',
-    'Yellow',
-    'Pink',
-    'LightSeaGreen',
-    'Aqua',
-    'PaleTurquoise',
-    'Turquoise',
-    'MediumTurquoise',
-    'MediumSlateBlue',
-    'Blue',
-    'BurlyWood',
-    'SandyBrown',
-    'Chocolate',
-    'Brown',
-    'Maroon',
-    'LightSalmon',
-  ];
-  
-  let number;
+  "Red",
+  "Yellow",
+  "Pink",
+  "LightSeaGreen",
+  "Aqua",
+  "PaleTurquoise",
+  "Turquoise",
+  "MediumTurquoise",
+  "MediumSlateBlue",
+  "Blue",
+  "BurlyWood",
+  "SandyBrown",
+  "Chocolate",
+  "Brown",
+  "Maroon",
+  "LightSalmon",
+];
+
+let number;
 
 const numberRandom = (typeNumber) => {
-    if (typeNumber === 'hexadecimal') {
-        number = Math.floor(Math.random() * 17);
-        return number.toString(16);
-    }
-    if (typeNumber === 'rgb') {
-        number = Math.floor(Math.random() * 256);
-        return number;
-    }
-    if (typeNumber === 'simple text') {
-        number = Math.floor(Math.random() * colorsText.length);
-        return number;
-    }
+  if (typeNumber === "hexadecimal") {
+    number = Math.floor(Math.random() * 17);
+    return number.toString(16);
+  }
+  if (typeNumber === "rgb") {
+    number = Math.floor(Math.random() * 256);
+    return number;
+  }
+  if (typeNumber === "simple text") {
+    number = Math.floor(Math.random() * colorsText.length);
+    return number;
+  }
 };
 
 const colorHexadecimalRandom = () => {
-    let color = `#${numberRandom('hexadecimal')}${numberRandom(
-        'hexadecimal'
-    )}${numberRandom('hexadecimal')}${numberRandom('hexadecimal')}${numberRandom(
-        'hexadecimal'
-    )}${numberRandom('hexadecimal')}`;
+  let color = `#${numberRandom("hexadecimal")}${numberRandom(
+    "hexadecimal"
+  )}${numberRandom("hexadecimal")}${numberRandom("hexadecimal")}${numberRandom(
+    "hexadecimal"
+  )}${numberRandom("hexadecimal")}`;
 
-    return color;
+  return color;
 };
 
 const colorRgbRandom = () => {
-    let color = `rgb(${numberRandom('rgb')}, ${numberRandom(
-        'rgb'
-    )}, ${numberRandom('rgb')})`;
+  let color = `rgb(${numberRandom("rgb")}, ${numberRandom(
+    "rgb"
+  )}, ${numberRandom("rgb")})`;
 
-    return color;
+  return color;
 };
 
 const colorSimpleTextRandom = () => {
-    let color = `${colorsText[numberRandom('simple text')]}`;
+  let color = `${colorsText[numberRandom("simple text")]}`;
 
-    return color;
+  return color;
 };
 
 btnColorSimple.addEventListener("click", () => {
@@ -77,24 +77,23 @@ btnColorRgb.addEventListener("click", () => {
   conmutador = "btnColorRgb";
 });
 
-btnColorRandom.addEventListener('click', () => {
-    let colors = '';
-    if (conmutador === 'btnColorSimple') {
-      colors = colorSimpleTextRandom();
-      document.body.style.background = `${colors}`;
-      colorText.textContent = colors.toUpperCase();
-    }
-  
-    if (conmutador === 'btnColorHexa') {
-      colors = colorHexadecimalRandom();
-      document.body.style.background = `${colors}`;
-      colorText.textContent = colors.toUpperCase();
-    }
-  
-    if (conmutador === 'btnColorRgb') {
-      colors = colorRgbRandom();
-      document.body.style.background = `${colorRgbRandom()}`;
-      colorText.textContent = colors.toUpperCase();
-    }
-  });
-  
+btnColorRandom.addEventListener("click", () => {
+  let colors = "";
+  if (conmutador === "btnColorSimple") {
+    colors = colorSimpleTextRandom();
+    document.body.style.background = `${colors}`;
+    colorText.textContent = colors.toUpperCase();
+  }
+
+  if (conmutador === "btnColorHexa") {
+    colors = colorHexadecimalRandom();
+    document.body.style.background = `${colors}`;
+    colorText.textContent = colors.toUpperCase();
+  }
+
+  if (conmutador === "btnColorRgb") {
+    colors = colorRgbRandom();
+    document.body.style.background = `${colorRgbRandom()}`;
+    colorText.textContent = colors.toUpperCase();
+  }
+});
